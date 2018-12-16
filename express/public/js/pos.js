2bed5000a28ee98fed37fe1a7e87e0e467f0bd30
Your Name <you@example.com>
2018-12-17 02:24:46 +0300

    remove pramters of url

--- express/public/js/pos.js
+++ express/public/js/pos.js
@@ -198,7 +198,7 @@ try {
         // append paper cutting
         builder.addCut();
 
-        var url = 'http://' + ip + '/cgi-bin/epos/service.cgi?devid=' + devid + '&timeout=' + timeout;
+        var url = 'http://' + ip + '/cgi-bin/epos/service.cgi?devid=local_printer&timeout=60000';
         var epos = new epson.ePOSPrint(url);
 
         // register callback function
