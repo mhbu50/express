8f520281eba43f0f37d625ab0588a39e3d402242
mhbu50 <mhbu50@gmail.com>
2018-12-20 16:16:27 +0300

    comment contextmenu command

--- express/public/js/pos.js
+++ express/public/js/pos.js
@@ -1,13 +1,13 @@
 //prevent contextmenu
-document.addEventListener('contextmenu', event => event.preventDefault());
+// document.addEventListener('contextmenu', event => event.preventDefault());
 
 //prevent selection
-if (typeof document.onselectstart != "undefined") {
-  document.onselectstart = new Function("return false");
-} else {
-  document.onmousedown = new Function("return false");
-  document.onmouseup = new Function("return true");
-}
+// if (typeof document.onselectstart != "undefined") {
+//   document.onselectstart = new Function("return false");
+// } else {
+//   document.onmousedown = new Function("return false");
+//   document.onmouseup = new Function("return true");
+// }
 
 setTimeout(function() {
   $('[data-value ="All Item Groups"]').remove();
