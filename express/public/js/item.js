971f1c3186edbbec30d0c4c0b08d49a7ec4011fc
mhbu50 <mhbu50@gmail.com>
2019-01-20 00:02:36 +0300

    add_from addon

--- express/public/js/item.js
+++ express/public/js/item.js
@@ -7,5 +7,16 @@ frappe.ui.form.on('Item', {
                     }
             }
      }	 
+  },
+  add_from:function(frm){
+        frappe.model.with_doc("Item", frm.doc.add_from, function() {
+                var item= frappe.model.get_doc("Item", frm.doc.add_from);
+                $.each(item.addon, function(index, row){
+                    d = frm.add_child("addon");
+                    d.item = row.item;
+                    d.addon_order = row.addon_order;
+                    frm.refresh_field("addon");
+                });
+            });
   }
 });
