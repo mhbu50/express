1c0d299aa68886ced0c65f4b2a39e9948ba30ff0
mhbu50 <mhbu50@gmail.com>
2019-01-28 18:31:05 +0300

    var me = this;

--- express/public/js/pos.js
+++ express/public/js/pos.js
@@ -335,7 +335,7 @@ try {
         // return "<button style='padding: 2px 5px; margin: 2px; font-weight: bold;  content: \"\" ; display: inline-block; margin-right: 5px; vertical-align: text-top; background-color: transparent; background-position : center center; background-repeat:no-repeat; background-image : url(" + data.image + "); class='btn' data-value='" + data.item_group+"'>" + data.item_group + "</button>";      
       }).join("");      
       this.search_item_group.find('.row').html(item_groups_html);
-      
+      var me = this;
       this.search_item_group.on('click', '.row button', function() {
         me.selected_item_group = $(this).attr('data-value');	
         $('.row button').removeClass("item-active");
