10412d8ed00d4c96724bce8f680428cbd48d29cf
mhbu50 <mhbu50@gmail.com>
2019-02-04 00:57:28 +0300

    comment numpad

--- express/public/js/pos.js
+++ express/public/js/pos.js
@@ -365,12 +365,12 @@ try {
         });
     });
             
-    $.fn.numpad.defaults.gridTpl = `<table class="table modal-content"></table>`;
-    $.fn.numpad.defaults.backgroundTpl = `<div class="modal-backdrop in"></div>`;
-    $.fn.numpad.defaults.displayTpl = `<input type="text" class="form-control" />`;
-    $.fn.numpad.defaults.buttonNumberTpl =  `<button type="button" class="btn btn-default"></button>`;
-    $.fn.numpad.defaults.buttonFunctionTpl = `<button type="button" class="btn" style="width: 100%;"></button>`;
-    $.fn.numpad.defaults.onKeypadCreate = function(){$(this).find(".done").addClass("btn-primary");};
+    // $.fn.numpad.defaults.gridTpl = `<table class="table modal-content"></table>`;
+    // $.fn.numpad.defaults.backgroundTpl = `<div class="modal-backdrop in"></div>`;
+    // $.fn.numpad.defaults.displayTpl = `<input type="text" class="form-control" />`;
+    // $.fn.numpad.defaults.buttonNumberTpl =  `<button type="button" class="btn btn-default"></button>`;
+    // $.fn.numpad.defaults.buttonFunctionTpl = `<button type="button" class="btn" style="width: 100%;"></button>`;
+    // $.fn.numpad.defaults.onKeypadCreate = function(){$(this).find(".done").addClass("btn-primary");};
     
     $("#text-basic").numpad({
         onKeypadOpen:function(){
