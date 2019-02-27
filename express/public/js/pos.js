447fb6183851973785a4ac226f0b510ff41dfd8c
Mohammed R Abukhamseen <mhbu50@gmail.com>
2019-02-28 00:45:14 +0300

    no print if is_return

--- express/public/js/pos.js
+++ express/public/js/pos.js
@@ -79,7 +79,7 @@ try {
         me.make_new_cart()
       })
     }
-         
+
     print_document(html){
       $('<iframe>', {
         name: 'myiframe',
@@ -92,9 +92,9 @@ try {
       window.frames['myiframe'].focus();
       window.frames['myiframe'].print();
 
-      setTimeout(() => { $(".printFrame").remove(); }, 1000)   
-    }         
-         
+      setTimeout(() => { $(".printFrame").remove(); }, 1000)
+    }
+
     send_to_printers(){
       function uniques(arr,feild) {
         var a = [];
@@ -102,6 +102,9 @@ try {
             if (a.indexOf(arr[i][feild]) === -1 && arr[i][feild] !== '')
                 a.push(arr[i][feild]);
         return a;
+        }
+        if(this.frm.doc.is_return && this.frm.doc.is_return == 1 ){
+          return;
         }
       debugger;
       var me = this;
@@ -239,7 +242,7 @@ try {
         var str_order = localStorage.getItem("order");
         var date = localStorage.getItem("date");
         //get a numeric value from str_order, put it in order
-        if ((str_order == null || str_order == "null") || 
+        if ((str_order == null || str_order == "null") ||
         (date == null || date != moment().format('MM D, YYYY')) ){
           var order = 0;
           var date = moment().format('MM D, YYYY');
@@ -255,7 +258,7 @@ try {
         localStorage.setItem("date", date);
       } catch (e) {
         console.log("e",e);
-        
+
         frappe.throw(__("LocalStorage is full , did not save order or date"))
       }
     }
@@ -267,12 +270,12 @@ try {
         method: "express.api.get_addon_list",
         freeze: true,
         callback: function (r) {
-          console.log("get_data_from_server");     
+          console.log("get_data_from_server");
           var addon_list = r.message;
           console.log("addon_list",addon_list);
-          
-          localStorage.setItem('addon_list', JSON.stringify(addon_list));   	
-          me.addon_list = addon_list;	 
+
+          localStorage.setItem('addon_list', JSON.stringify(addon_list));
+          me.addon_list = addon_list;
         }
       });
       console.log("onload this",this);
@@ -289,33 +292,33 @@ try {
         method: "express.api.get_items_order",
         freeze: true,
         callback: function (r) {
-  
+
           var items_order = r.message;
           console.log("items_order",items_order);
-          
-          localStorage.setItem('items_order', JSON.stringify(items_order));   	
-          me.items_order = items_order;	 
+
+          localStorage.setItem('items_order', JSON.stringify(items_order));
+          me.items_order = items_order;
         }
       });
       if(localStorage.getItem("items_order") !== "undefined"){
       this.items_order =  JSON.parse(localStorage.getItem("items_order"));
-      }      
+      }
     }
 
     add_to_cart(){
       super.add_to_cart();
-      if(cur_pos.frm.doc.addons.length >0 && 
-        cur_pos.frm.doc.addons.findIndex(p => p.parent_item == this.items[0].name && p.addon == "قياسي - Standard") > 0){      
+      if(cur_pos.frm.doc.addons.length >0 &&
+        cur_pos.frm.doc.addons.findIndex(p => p.parent_item == this.items[0].name && p.addon == "قياسي - Standard") > 0){
       var addon_item_index = cur_pos.frm.doc.addons.findIndex(p => p.parent_item == this.items[0].name && p.addon == "قياسي - Standard");
-      console.log("addon_item_index",addon_item_index);      
-      cur_pos.frm.doc.addons[addon_item_index].parent_qty = parseInt(cur_pos.frm.doc.addons[addon_item_index].parent_qty) +1;  
-      cur_pos.rerender_pos_bill_item_new();          
+      console.log("addon_item_index",addon_item_index);
+      cur_pos.frm.doc.addons[addon_item_index].parent_qty = parseInt(cur_pos.frm.doc.addons[addon_item_index].parent_qty) +1;
+      cur_pos.rerender_pos_bill_item_new();
       }
     }
 
     get_sorted_item_groups(){
       let list = [];
-      $.each(cur_pos.pos_profile_data.item_groups, function(i, data) {                 
+      $.each(cur_pos.pos_profile_data.item_groups, function(i, data) {
         list[i] = { "item_group":data.item_group,"image":data.image};
       });
       return list;
@@ -324,25 +327,25 @@ try {
       super.make_search();
       sorted_item_groups = this.get_sorted_item_groups();
       let item_groups_html = sorted_item_groups.map(function(data) {
-      
+
         return `<button class='button' data-value= '${data.item_group}' style='text-align: center; text-decoration: none; display: inline-block;
          font-weight: bold; margin: 4px 2px; cursor: pointer;'>
          <span style='background: url(${data.image}) no-repeat;float: left;
           width: 32px; height: 32px; margin-right: 10px'></span>${data.item_group}</button>`
-      
-        // return "<button style='padding: 2px 5px; margin: 2px; font-weight: bold;  content: \"\" ; display: inline-block; margin-right: 5px; vertical-align: text-top; background-color: transparent; background-position : center center; background-repeat:no-repeat; background-image : url(" + data.image + "); class='btn' data-value='" + data.item_group+"'>" + data.item_group + "</button>";      
-      }).join("");      
-      //this.search_item_group.find('.row').html(item_groups_html); 
+
+        // return "<button style='padding: 2px 5px; margin: 2px; font-weight: bold;  content: \"\" ; display: inline-block; margin-right: 5px; vertical-align: text-top; background-color: transparent; background-position : center center; background-repeat:no-repeat; background-image : url(" + data.image + "); class='btn' data-value='" + data.item_group+"'>" + data.item_group + "</button>";
+      }).join("");
+      //this.search_item_group.find('.row').html(item_groups_html);
 	this.search_item_group.parent().html(item_groups_html);
       var me = this;
       //this.search_item_group.on('click', '.row button', function() {
 	$(".pos-bill-header").on('click', 'button', function() {
         console.log("on('click',");
-        
-        me.selected_item_group = $(this).attr('data-value');	
+
+        me.selected_item_group = $(this).attr('data-value');
         $('.row button').removeClass("item-active");
         $(this).addClass("item-active");
-        
+
         me.page_len = 20;
         me.items = me.get_items();
         me.make_item_list();
@@ -352,7 +355,7 @@ try {
     render_selected_item(){
       super.render_selected_item();
       $(".pos-selected-item-action").prepend('<button type="button" id="addons" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Modifier</button>');
-      
+
       $("#addons").click(function() {
         var remain = $("#remain").text();
         $(".numbers_dialog").each(function(i) {
@@ -364,7 +367,7 @@ try {
             }
         });
     });
-            
+
     frappe.require('assets/express/js/jquery.numpad.js', function() {
     $.fn.numpad.defaults.gridTpl = `<table class="table modal-content"></table>`;
     $.fn.numpad.defaults.backgroundTpl = `<div class="modal-backdrop in"></div>`;
@@ -372,7 +375,7 @@ try {
     $.fn.numpad.defaults.buttonNumberTpl =  `<button type="button" class="btn btn-default"></button>`;
     $.fn.numpad.defaults.buttonFunctionTpl = `<button type="button" class="btn" style="width: 100%;"></button>`;
     $.fn.numpad.defaults.onKeypadCreate = function(){$(this).find(".done").addClass("btn-primary");};
-    
+
     $("#text-basic").numpad({
         onKeypadOpen:function(){
             var keypad = $(this);
@@ -389,7 +392,7 @@ try {
                 var addons = cur_pos.item_data.filter(obj => {
                   return obj.item_group === "اضافات - Additions To The Sandwich"
                 });
-		
+
                 var result = "";
                 for (var addon in addons){
                   result = result +
@@ -410,7 +413,7 @@ try {
                 </div>
                 `;
                 $("#new_nump_addons").prepend(addons_template);
-                $("#text-basic").val("#")                
+                $("#text-basic").val("#")
                 }
             });
         }
@@ -442,7 +445,7 @@ try {
     };
 
     if (call_api) {
-        var addons_template =cur_pos.get_rendered_addons(addons,item_code);        
+        var addons_template =cur_pos.get_rendered_addons(addons,item_code);
         $("#new_nump_addons").prepend(addons_template);
         var total = 0;
         $("#new_nump_addons").find("h3").each(function(i) {
@@ -465,7 +468,7 @@ try {
         var remain = parseInt($("#remain").text()) - parseInt(this.innerHTML);
         var valueForSelectedItem = this.innerHTML;
         if (remain < 0) {
-            frappe.msgprint("Quantity will be negative") 
+            frappe.msgprint("Quantity will be negative")
         } else {
 		$("#remain").text(remain);
 		var addons ;
@@ -503,13 +506,13 @@ try {
             //   <input type='hidden' name='alarm' value='False'/>
             //   <input class='custom-checkbox-input' name='alarm' value='${addons[addon].name}' type='checkbox'>
             //   <span class='custom-checkbox-text'>${addons[addon].name}</span>
-            // </label>`;            
+            // </label>`;
 
             // result = result +
             // `<span class="button-checkbox">
             // <button type="button" class="btn" data-color="primary">${addons[addon].name}</button>
             // <input type="checkbox"  value="${addons[addon].name} class="hidden"/>
-            // </span>`            
+            // </span>`
         };
 
         var addons_template = ` <div class="modal-body">
@@ -541,7 +544,7 @@ try {
                       icon: 'glyphicon glyphicon-unchecked'
                   }
               };
-  
+
           // Event Handlers
           $button.on('click', function () {
               $checkbox.prop('checked', !$checkbox.is(':checked'));
@@ -551,19 +554,19 @@ try {
           $checkbox.on('change', function () {
               updateDisplay();
           });
-  
+
           // Actions
           function updateDisplay() {
               var isChecked = $checkbox.is(':checked');
-  
+
               // Set the button's state
               $button.data('state', (isChecked) ? "on" : "off");
-  
+
               // Set the button's icon
               $button.find('.state-icon')
                   .removeClass()
                   .addClass('state-icon ' + settings[$button.data('state')].icon);
-  
+
               // Update the button's color
               if (isChecked) {
                   $button
@@ -576,12 +579,12 @@ try {
                       .addClass('btn-default');
               }
           }
-  
+
           // Initialization
           function init() {
-  
+
               updateDisplay();
-  
+
               // Inject the icon if applicable
               if ($button.find('.state-icon').length == 0) {
                   $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i>');
@@ -626,7 +629,7 @@ try {
          addon["parent_item"] = $("#new_nump_addons").attr("data-parent-item");
          addon["addons"] = [{ "addon": "قياسي - Standard", "price": 0 }];
          addons_list.push(addon);
-        }                      	
+        }
 
             var final_array_items = [];
             for (var i = 0; i < addons_list.length; i++) {
@@ -655,23 +658,23 @@ try {
         }
       );
     }
-    
-    rerender_pos_bill_item_new(){      	
+
+    rerender_pos_bill_item_new(){
       var addons = cur_pos.frm.doc.addons;
       var array_addons_for_display = [];
       var $items = this.wrapper.find(".items").empty();
-  
+
       var groupBy = function(xs, key) { return xs.reduce(function(rv, x) { (rv[x[key]] = rv[x[key]] || []).push(x); return rv; }, {}); };
-      var parentsGroup =groupBy(addons,'parent_item') 
-      var unique =[] 
+      var parentsGroup =groupBy(addons,'parent_item')
+      var unique =[]
       for (var key in parentsGroup) {
         var item = groupBy(parentsGroup[key],'group_id')
         for(var key2 in item){
           var obj = {addon:[],parent_qty:item[key2][0].parent_qty,parent_item:key,group_id:key2}
-          obj['addon'] = item[key2].map((function (row) { return row.addon })); 
-          unique.push(obj) } 
+          obj['addon'] = item[key2].map((function (row) { return row.addon }));
+          unique.push(obj) }
         }
-  
+
       $.each(cur_pos.frm.doc.items || [], function (i, d) {
         $(frappe.render_template("pos_bill_item_new", {
           item_code: d.item_code,
@@ -687,7 +690,7 @@ try {
         })).appendTo($items);
       });
     }
-          
+
     clear_addons(){
       var items = cur_pos.frm.doc.items;
       var addons = cur_pos.frm.doc.addons;
@@ -702,12 +705,12 @@ try {
             }else{
               item_dict[addons[i]["parent_item"]] = parseInt(item_dict[addons[i]["parent_item"]])+parseInt(addons[i]["parent_qty"]);
               item_group_dict[addons[i]["parent_item"]].push(parseInt(addons[i]["group_id"]));
-            }		
+            }
           }
         }else{
           item_dict[addons[i]["parent_item"]] = parseInt(addons[i]["parent_qty"]);
           item_group_dict[addons[i]["parent_item"]] = [parseInt(addons[i]["group_id"])];
-  
+
         }
       };
       for (var x in item_dict) {
@@ -730,9 +733,9 @@ try {
         }
       };
           }
-  
+
     }
-          
+
     get_rendered_addons(addons,item_code){
       if(addons.length >0){
         var data = addons;
@@ -774,12 +777,12 @@ try {
                       "parent_item": data[row].parent_item
                       })
                 }
-            }          
+            }
           }
 
         for(var row in cleand_addon){
             var addons_template="";
-            var x = []; 
+            var x = [];
             for (var i =  cleand_addon[row].addon.length - 1; i >= 0; i--) {
               x.push(cleand_addon[row].addon[i].name);
             }
@@ -795,7 +798,7 @@ try {
                 }
             }
 
-            parent_template = parent_template+` 
+            parent_template = parent_template+`
               <div class="modal-body">
                   <h3 class="collapsible-custom" data-value =${cleand_addon[row].parent_qty}>Total Number of Items ${cleand_addon[row].parent_qty}</h3>
                   <div class="items-for-addons" data-group-qty =${cleand_addon[row].parent_qty}>
@@ -806,11 +809,11 @@ try {
         return parent_template
       }
     }
-      
+
     hide_section(addon){
       $(addon).siblings(".items-for-addons").slideToggle("fast");
       $(addon).find("span").toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
-    }	
+    }
   }
   erpnext.pos.PointOfSale = PointOfSale;
 
@@ -823,7 +826,7 @@ try {
 //  $(".item-cart-items").find(".pos-bill-item, .active").each(function(index,g) {
 //     if($(g).hasClass( "active" )){
 //       idx = g;
-//     }    
+//     }
 //   });
 
 //   var idx;
@@ -832,28 +835,28 @@ try {
 //     const element = cart_list[i];
 //     if($(cart_list[i]).hasClass( "active" )){
 //       idx = g;
-//     }     
+//     }
 //   }
 //   console.log(idx);
-  
+
 }
 // function dddd(g) {
 //   console.log($(g).hasClass("active"));
 // }
 
 // function mapOrder (array, order, key) {
-  
+
 //   array.sort( function (a, b) {
 //     var A = a[key], B = b[key];
-    
+
 //     if (order.indexOf(A) > order.indexOf(B)) {
 //       return 1;
 //     } else {
 //       return -1;
 //     }
-    
+
 //   });
-  
+
 //   return array;
 // };
 
