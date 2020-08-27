6139999660df665e19272cd30e81efb296c4352e
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2020-08-27 16:15:19 +0500

    Fix: Material Request UOM   UOM should be same as in Order Item List

---
+++ express/public/js/material_request.js
@@ -0,0 +1,31 @@
+
+
+
+frappe.ui.form.on('Material Request Item', {
+    item_code: function (frm, cdt, cdn) {  
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.item_code){
+            frappe.call('express.api.get_order_item_uom', {item_code: row.item_code})
+            .then( r => {
+                if (r.message){
+                    row.uom = r.message.uom;
+                    frm.refresh_field('items')
+                }
+            })
+        }
+    },
+    uom: function (frm, cdt, cdn) {  
+        let row = frappe.get_doc(cdt, cdn)
+        if (row.item_code){
+            frappe.call('express.api.get_order_item_uom', {item_code: row.item_code})
+            .then( r => {
+                if (r.message){
+                    row.uom = r.message.uom;
+                    frm.refresh_field('items')
+                }
+            })
+        }
+    }
+})
+
+// frappe.ui.form.off("Material Request Item", "qty")
