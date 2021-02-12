ee7479b42e27c3e0f81e048bcb74738620299926
afifaa0 <afifasaleem77@gmail.com>
2021-02-12 16:04:47 +0500

    update: update in list

--- express/events/background_update.py
+++ express/events/background_update.py
@@ -1,12 +1,47 @@
 import frappe
+import json
 from frappe.utils.background_jobs import enqueue
+
 
 @frappe.whitelist()
 def submit(doctype, docname):
     doc = frappe.get_doc(doctype, docname)
-    enqueue(doc.submit, job_name= "Submitting {}".format(doc.name), queue="long")
+    enqueue(doc.submit, job_name="Submitting {} {}".format(doctype, docname), queue="long")
+
 
 @frappe.whitelist()
 def cancel(doctype, docname):
     doc = frappe.get_doc(doctype, docname)
-    enqueue(doc.cancel, job_name= "Cancelling {}".format(doc.name), queue="long")
+    enqueue(doc.cancel, job_name="Cancelling {} {}".format(doctype, docname), queue="long")
+
+
+@frappe.whitelist()
+def submit_multiple(doctype, docs):
+    docs = json.loads(docs)
+    result = []
+    flag = 0
+    for row in docs:
+        doc = frappe.get_doc(doctype, row["name"])
+        if doc.docstatus != 0:
+            result.append(doc.name)
+        else:
+            flag = 1
+            enqueue(doc.submit, job_name="Submitting {} {}".format(doctype, doc.name), queue="long")
+    result.append(flag)
+    return result
+
+
+@frappe.whitelist()
+def cancel_multiple(doctype, docs):
+    docs = json.loads(docs)
+    result = []
+    flag = 0
+    for row in docs:
+        doc = frappe.get_doc(doctype, row["name"])
+        if doc.docstatus != 1:
+            result.append(doc.name)
+        else:
+            flag = 1
+            enqueue(doc.cancel, job_name="Cancelling {} {}".format(doctype, doc.name), queue="long")
+    result.append(flag)
+    return result
