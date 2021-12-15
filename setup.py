0d6a7596464e2a78b6f6f9f4a5385a367c4a6475
Mohammed Redah <mhbu50@gmail.com>
2021-12-15 12:42:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from setuptools import setup, find_packages
-try: # for pip >= 10
-	from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-	from pip.req import parse_requirements
 import re, ast
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in express/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+	
 with open('express/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='express',
 	version=version,
-	description='sandwich App',
-	author='Accurate Systems',
+	description='express form for ERPNext',
+	author='accuratesystems.com.sa',
 	author_email='info@accuratesystems.com.sa',
 	packages=find_packages(),
 	zip_safe=False,
